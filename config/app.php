d8263aac90726fbc394359dd6a8e15fdde4d1d43
Aqib <aqibr777@gmail.com>
2022-11-16 00:38:45 +0500

    5. creating test tenants

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Providers\TenancyServiceProvider;
+
 return [
 
     /*
@@ -174,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        TenancyServiceProvider::class
 
     ],
 
